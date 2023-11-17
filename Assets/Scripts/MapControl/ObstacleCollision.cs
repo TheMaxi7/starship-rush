d225bb3861523e9eb3f8c19ff20b77c07017cebe
Davide Altamura <davide.altamura97@gmail.com>
2023-11-17 21:58:00 +0100

    Collisions not working properly

---
+++ Assets/Scripts/MapControl/ObstacleCollision.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleCollision : MonoBehaviour
+{
+    public AudioSource collisionFX;
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+        //collisionFX.Play();
+        
+    //}
+}
