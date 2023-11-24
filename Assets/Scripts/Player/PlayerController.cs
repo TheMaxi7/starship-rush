0412aec59822cbbb13c642f34624a42badefc2aa
Davide Altamura <davide.altamura97@gmail.com>
2023-11-24 21:51:26 +0100

    Added vertical movement
    Clamped vertical and horizontal movement, added animations up and down, fixed camera respawn

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -10,25 +10,32 @@ public class PlayerController : MonoBehaviour
     public static float forwardSpeed = 10f;
     public float maxSpeed = 40f;
     public float horizontalSpeed;
+    public float verticalSpeed;
     public ParticleSystem explosionParticle;
     public AudioSource explosionFX;
     float horizontal;
+    float vertical;
     private Animator animator;
-
+    public float minY = 0.8f;
+    public float maxY = 7f;
+    public float minX = -10f;
+    public float maxX = 10f;
     void Start()
     {
         rb = GetComponent<Rigidbody>();  
         animator = GetComponent<Animator>();
+        float clampedVertical = Mathf.Clamp(rb.position.y, minY, maxY);
     }
 
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
-    
+        vertical = Input.GetAxis("Vertical");
+
         if (forwardSpeed < maxSpeed)
             forwardSpeed += 0.2f * Time.deltaTime;
 
-        direction = new Vector3(horizontal * horizontalSpeed, 0f, forwardSpeed);
+        direction = new Vector3(horizontal * horizontalSpeed, vertical * verticalSpeed, forwardSpeed);
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             animator.SetBool("Left", true);
@@ -45,6 +52,23 @@ public class PlayerController : MonoBehaviour
         else
         {
             animator.SetBool("Right", false);
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            animator.SetBool("Up", true);
+        }
+        else
+        {
+            animator.SetBool("Up", false);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            animator.SetBool("Down", true);
+        }
+        else
+        {
+            animator.SetBool("Down", false);
         }
     }
 
@@ -63,7 +87,13 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-   
+        
+        float clampedVertical = Mathf.Clamp(rb.position.y, minY, maxY);
+        float clampedHorizontal = Mathf.Clamp(rb.position.x, minX, maxX);
+        rb.position = new Vector3(clampedHorizontal, clampedVertical, rb.position.z);
+
+        direction = new Vector3(horizontal * horizontalSpeed, vertical * verticalSpeed, forwardSpeed);
+
         rb.velocity = direction;
     }
 
--- Assets/Scripts/UI/Events.cs
+++ Assets/Scripts/UI/Events.cs
@@ -6,20 +6,11 @@ using UnityEngine.SceneManagement;
 public class Events : MonoBehaviour
 {
     public GameObject player;
+    public GameObject camera;
     public GameObject continuePanel;
     public void ContinueGame()
     {   
-        Rigidbody playerRigidbody = player.GetComponent<Rigidbody>();
-        player.transform.position = new Vector3(0f, 1.66f, GenerateLevel.zPos - 25f);
-        player.transform.rotation = Quaternion.identity;
-        playerRigidbody.velocity = Vector3.zero;
-        playerRigidbody.angularVelocity = Vector3.zero;
-        playerRigidbody.MovePosition(new Vector3(0f, 1.66f, GenerateLevel.zPos - 25f));
-        playerRigidbody.MoveRotation(Quaternion.identity);
-
-        player.SetActive(true);
-        PlayerManager.gameOver = false;
-        continuePanel.SetActive(false);
+        ResetComponents();
         uiControl.heartCount--;
     }
 
@@ -35,5 +26,21 @@ public class Events : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void ResetComponents()
+    {
+        Rigidbody playerRigidbody = player.GetComponent<Rigidbody>();
+        player.transform.position = new Vector3(0f, 1.66f, GenerateLevel.zPos - 25f);
+        player.transform.rotation = Quaternion.identity;
+        camera.transform.position = new Vector3(0f, 2.81f, GenerateLevel.zPos - 25f - 6.72f);
+        playerRigidbody.velocity = Vector3.zero;
+        playerRigidbody.angularVelocity = Vector3.zero;
+        playerRigidbody.MovePosition(new Vector3(0f, 1.66f, GenerateLevel.zPos - 25f));
+        playerRigidbody.MoveRotation(Quaternion.identity);
+
+        player.SetActive(true);
+        PlayerManager.gameOver = false;
+        continuePanel.SetActive(false);
     }
 }
